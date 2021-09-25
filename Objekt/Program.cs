4edc7c1865b0ec63cdb373f366e8786779fc977d
Sergey Richter <Sergey Richter@LAPTOP-39P1DN0S>
2021-09-26 00:23:04 +0200

    Projektdateien hinzufügen.

---
+++ Objekt/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var belegpositionen = new List<BelegPosition>();
+            var belegposition = new BelegPosition();
+            belegposition.Artikelnummer = "1234567";
+            belegposition.AuspraegungID = 24742842;
+            belegposition.Matchcode = "ehehhq00";
+            belegpositionen.Add(belegposition);
+            foreach (var item in belegpositionen)
+            {
+                Console.WriteLine(item.Artikelnummer);
+                Console.WriteLine(item.AuspraegungID);
+                Console.WriteLine(item.Matchcode);
+                Console.ReadKey();
+            }
+
+        }
+
+    }
+
+    public class BelegPosition
+    {
+        public string Artikelnummer { get; set; }
+        public int AuspraegungID { get; set; }
+        public string Matchcode { get; set; }
+    }
+}
+          
+        
